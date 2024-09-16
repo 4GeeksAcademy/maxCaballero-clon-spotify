87af4180f842fbf194430e2cc955e73aae177789
Christian Caballero <xtian.cr78@gmail.com>
2024-09-16 01:30:38 +0000

    reproductor

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import Header from "./Header.js";
+import Body from "./Body.js";
+import Footer from "./Footer.js";
+
 
 //create your first component
 const Home = () => {
-  return <div className="text-center"></div>;
+  return (
+    <div className="text-center">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
